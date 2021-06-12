f363b8139b7707d55cc00f687791994b93070a92
Stas Prohetamine <prohetamine@gmail.com>
2021-06-12 04:36:54 +0300

    publish

--- src/navigation.jsx
+++ src/navigation.jsx
@@ -280,7 +280,7 @@ const Navigation = () => {
         icon={[telegram, telegramBlack]}
         onClick={() => window.open('https://t.me/prohetamine_emojiart')}
       />
-      <By>By Prohetamine 2021</By>
+    <By>By <a href='https://github.com/prohetamine'>Prohetamine</a> 2021</By>
     </Body>
   )
 }
